b9e54d382c5dc6984ff3d9ef154002abf2d4313e
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-03-23 01:45:55 -0500

    styling and shark details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import './App.css';
+import Shark from './Shark.js';
+import AutomaticStop from './Automatic_Stop.mp3';
 
 class App extends Component {
 
@@ -16,8 +19,17 @@ class App extends Component {
 		let context = new AudioContext();
 		let analyser = context.createAnalyser();
 
-		let canvas = this.refs.analyzerCanvas;
+		let canvas = this.refs.analyserCanvas;
+		canvas.style.background = "blueviolet";
+
+		let dpi = window.devicePixelRatio;
+		let scale = dpi;
+
+		canvas.width = canvas.width * scale;
+		canvas.height = canvas.height * scale
+
 		let ctx = canvas.getContext('2d');
+		ctx.scale(scale, scale)
 
 		let audio = this.refs.audio;
 		audio.crossOrigin = "anonymous";
@@ -28,18 +40,56 @@ class App extends Component {
 		audioSrc.connect(context.destination);
 		
 		function renderFrame() {
+			let fishData = new Uint8Array(Shark)
 			let freqData = new Uint8Array(analyser.frequencyBinCount)
+			for(let i; i < fishData.length; i++) {
+				for (let j = 0; j < freqData.length; j++) {
+					freqData[j] *= fishData[i];
+					console.log(freqData)
+				}
+			}
 			requestAnimationFrame(renderFrame)
 			analyser.getByteFrequencyData(freqData)
 			ctx.clearRect(0,0, canvas.width, canvas.height)
-			console.log(freqData)
-			ctx.fillStyle = 'green';
-			let bars = 100;
+			ctx.fillStyle = 'deeppink';
+			// 'z-indez' shark details
+			ctx.globalCompositeOperation='destination-over';
+			ctx.beginPath(); // shark eye
+			ctx.arc(250, 130, 4, 0, Math.PI * 2, true);
+			ctx.stroke()
+
+			// shark gills
+			ctx.beginPath()
+			ctx.arc(105, 140, 35, Math.PI*1.75, Math.PI*2, false);
+			ctx.stroke()
+			ctx.beginPath()
+			ctx.arc(115, 140, 35, Math.PI*1.75, Math.PI*2, false);
+			ctx.stroke()
+			ctx.beginPath()
+			ctx.arc(125, 140, 35, Math.PI*1.75, Math.PI*2, false);
+			ctx.stroke();
+
+			// shark teeth
+			ctx.beginPath();
+			ctx.moveTo(280, 145);
+			ctx.lineTo(275, 140);
+			ctx.lineTo(270, 145);
+			ctx.lineTo(265, 140);
+			ctx.lineTo(260, 145);
+			ctx.lineTo(255, 140);
+			ctx.lineTo(250, 145);
+			ctx.lineTo(245, 140);
+			ctx.lineTo(240, 145);
+			ctx.lineTo(235, 140);
+			ctx.lineTo(230, 145);
+			ctx.stroke();
+
+			let bars = 500;
 			for (var i = 0; i < bars; i++) {
 				let bar_x = i * 3;
 				let bar_width = 2;
-				let bar_height = -((freqData[i] / 2) + 1);
-				ctx.fillRect(bar_x, canvas.height, bar_width, bar_height)
+				let bar_height = -(((freqData[i]) / 10) + fishData[i]);
+				ctx.fillRect(bar_x, canvas.height-50, bar_width, bar_height)
 			}
 		};
 		renderFrame()
@@ -48,21 +98,21 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>
-					The Strokes: Automatic Stop
-				</h1>
 				<div id="mp3_player">
 					<div id="audio_box">
+						<canvas
+							ref="analyserCanvas"
+							id="analyser"
+							/>			
 						<audio
 						ref="audio"
 						autoPlay={true}
 						controls={true}
-						src={"https://p.scdn.co/mp3-preview/e4a8f30ca62b4d2a129cc4df76de66f43e12fa3f?cid=null"}
+						src={AutomaticStop}
 						/>
-						<canvas
-							ref="analyzerCanvas"
-							id="analyzer"
-						/>			
+						<h2>
+							Automatic Stop - The Strokes
+						</h2>
 					</div>
 				</div>
 			</div>
---
+++ src/Shark.js
@@ -0,0 +1,14 @@
+let Shark = []
+for (let i = 0; i < 30; i++) {
+    Shark.push(Math.sqrt(i)*5 + 10)
+}
+
+for (let i = 10; i < 40; i++) {
+    Shark.push((-(i*2.6) - 110))
+}
+
+for (let i = 50; i >= 7; i--) {
+    Shark.push(Math.sqrt(i)*4 + 15)
+}
+console.log(Shark)
+export default Shark;
